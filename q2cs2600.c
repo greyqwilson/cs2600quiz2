4c4d2a98b3b9b57424708469485e0f1727628f40
Greyson Wilson <gqwilson@cpp.edu>
2022-02-28 16:21:36 -0800

    Initial commit - P1 outlined

---
+++ q2cs2600.c
@@ -0,0 +1,13 @@
+//This file is meant to serve as the main file for Quiz 2 in Prof. Davarpanah's CS2600 class
+//Author: Greyson Wilson
+//Quiz 2: Problem 1 Restuarant Bill - Computes the tax and tip of a restaurant bill
+//Inputs: Tax amount and tip (as percentages) through main()
+//Outputs: Tax amount, tip amount, and total price of meal
+
+
+#include <stdio.h>
+
+
+int main(float taxPct, float tipPct){
+    
+}
